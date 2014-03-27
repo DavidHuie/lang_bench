bf63f7e7c77e3868f2ffb94cda27964af95058ed
Michael Batchelor <batchelor@nationbuilder.com>
2014-03-26 17:59:19 -0700

    Java implementation

---
+++ java/src/main/java/com/nationbuilder/sendgrid/SendgridReceiver.java
@@ -0,0 +1,36 @@
+package com.nationbuilder.sendgrid;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+public class SendgridReceiver extends HttpServlet {
+
+	private static final long serialVersionUID = 4997832136717624098L;
+
+	private static final String SENDGRID_QUEUE = "test_list";
+	private static final String HEADER = "test";
+
+	private static final JedisPool REDIS = new JedisPool("localhost", 6379);
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String data = req.getHeader(HEADER);
+		Jedis redis = null;
+		try {
+			redis = REDIS.getResource();
+			redis.lpush(SENDGRID_QUEUE, data);
+		} finally {
+			if (redis != null)
+				REDIS.returnResource(redis);
+		}
+	}
+
+}
