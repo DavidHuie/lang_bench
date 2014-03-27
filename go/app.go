609fe4550a378141d24a92b69aaab47be87297b0
David Huie <david@nationbuilder.com>
2014-03-26 17:10:56 -0700

    Use a redis pool

--- go/app.go
+++ go/app.go
@@ -8,19 +8,23 @@ import (
 )
 
 const (
-	REDIS_KEY = "test_list"
-	PORT      = ":3001"
+	REDIS_KEY       = "test_list"
+	REDIS_POOL_SIZE = 20
+	PORT            = ":3001"
 )
 
-var RedisConn redis.Conn
+var RedisPool *redis.Pool
 
 func httpHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	value := r.Form["test"][0]
-	response, _ := RedisConn.Do("lpush", REDIS_KEY, value)
-	if response != nil {
-		fmt.Fprintf(w, strconv.Itoa(int(response.(int64))))
+	conn := RedisPool.Get()
+	defer conn.Close()
+	response, err := conn.Do("lpush", REDIS_KEY, value)
+	if err != nil {
+		fmt.Println(err.Error())
 	}
+	fmt.Fprintf(w, strconv.Itoa(int(response.(int64))))
 }
 
 func main() {
@@ -31,6 +35,10 @@ func main() {
 	}
 }
 
+func newConn() (redis.Conn, error) {
+	return redis.Dial("tcp", ":6379")
+}
+
 func init() {
-	RedisConn, _ = redis.Dial("tcp", ":6379")
+	RedisPool = redis.NewPool(newConn, REDIS_POOL_SIZE)
 }
